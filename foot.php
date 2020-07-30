f974f0f40a5994e4392b1022ba48a522a1416bae
blog-ph <68992106+blog-ph@users.noreply.github.com>
2020-07-30 11:48:54 +0200

    Add files via upload

---
+++ foot.php
@@ -0,0 +1,50 @@
+   <footer class="pt-5 mt-2">
+        <div class="container">
+            <section class="footer-content text-center">
+                <h2 class="">Your Library in your Pocket</h2>
+                <p>Turn Your phone or tablet into a book with free kindle app </p>
+                <a href="#contact"><button class="btn btn-success mt-3">Contact Us</button></a>
+           <!--      <p class="footer-social-links text-center my-4">
+                    <a href="#" class="icon-fb"><i class="mdi mdi-facebook-box"></i></a>
+                    <a href="#" class="icon-twitter"><i class="mdi mdi-twitter-box"></i></a>
+                    <a href="#" class="icon-insta"><i class="mdi mdi-instagram"></i></a>
+                    <a href="#" class="icon-behance"><i class="mdi mdi-behance"></i></a>
+                    <a href="#" class="icon-dribbble"><i class="mdi mdi-dribbble-box"></i></a>
+                    <a href="#" class="icon-github"><i class="mdi mdi-github-circle"></i></a>
+                </p> -->
+            </section>
+            <section><p class="my-4"><i class="mdi mdi-globe-model"></i> C/ Cuesta del Álamo, 65 29570 Cártama ,(spain)</p>
+                <p><i class="mdi mdi-email"></i> Email : AlulayCerdaCordero@teleworm.com</p>
+                <p><i class="mdi mdi-phone"></i> Phone : +34 976 56 23 67 </p>
+            </section>
+            <nav class="navbar navbar-light bg-transparent navbar-expand d-block d-sm-flex text-center">
+                <span class="navbar-text">&copy; kindle app. All rights reserved.</span>
+                <div class="navbar-nav ml-auto justify-content-center">
+                    <a href="refund.php" class="nav-link">Refund policy </a>
+                    <a href="term.php" class="nav-link">Terms</a>
+                    <a href="privacy-policy.php" class="nav-link">Privacy</a>
+                </div>
+            </nav>
+        </div>
+    </footer>
+    <div id="demoVideoLightbox" class="lightbox" onclick="hideVideo('video','youtube')">
+        <div class="lightbox-container">  
+          <div class="lightbox-content">
+            
+            <button data-close="lightbox" class="lightbox-close">
+              Close | ✕
+            </button>
+            <div class="video-container">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/eTlZLNws6zc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>      
+            
+          </div>
+        </div>
+      </div>
+    <script src="src/vendors/jquery/dist/jquery.min.js"></script>
+    <script src="src/vendors/popper.js/dist/umd/popper.min.js"></script>
+    <script src="src/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="src/js/miri-ui-kit.js"></script>
+</body>
+
+</html>
---
+++ head.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="img/fav.png" type="image/x-icon">
+    <title>Kindle App - Read eBooks, comics & more</title>
+
+    <!-- Vendor css -->
+    <link rel="stylesheet" href="src/vendors/@mdi/font/css/materialdesignicons.min.css">
+
+    <!-- Base css with customised bootstrap included -->
+    <link rel="stylesheet" href="src/css/miri-ui-kit-free.css">
+
+    <!-- Stylesheet for demo page specific css -->
+    <link rel="stylesheet" href="./assets/css/demo.css">
+</head>
+
+<body>
+    <header class="miri-ui-kit-header landing-header header-bg-2">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-transparent fixed-on-scroll">
+            <div class="container">
+                <a class="navbar-brand" href="index.html"><!-- <img src="assets/images/logo.svg" alt="logo"> -->Kindle App</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#miriUiKitNavbar"
+                    aria-controls="navbarSupportedContent2" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="mdi mdi-menu"></span>
+                </button>
+            
+                <div class="collapse navbar-collapse" id="miriUiKitNavbar">
+                    <div class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php" target="_blank">Home</span></a>
+                        </li>
+            
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php#about">About Us</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a href="index.php#features" class="nav-link ">
+                                Our features
+                            </a>
+                            <!-- <div class="dropdown-menu dropdown-menu-right ">
+                                <a href="landing.html" class="dropdown-item"><i class="dropdown-item-icon mdi mdi-shape-outline"></i>Landing Page</a>
+                                <a href="login.html" class="dropdown-item"><i class="dropdown-item-icon mdi mdi-lock-outline"></i>Login Page</a>
+                                <a href="profile.html" class="dropdown-item"><i class="dropdown-item-icon mdi mdi-shield-account-outline"></i>Profile Page</a>
+                            </div> -->
+                        </li>
+
+                         <li class="nav-item">
+                            <a class="nav-link" href="index.php#prices">Prices</a>
+                        </li>
+                         <li class="nav-item">
+                            <a class="nav-link" href="index.php#reviews">Reviews</a>
+                        </li>
+                         <li class="nav-item">
+                            <a class="nav-link" href="index.php#contact">Contact Us</a>
+                        </li>
+            
+                        <!-- <li class="nav-item">
+
+                            <a class="nav-link nav-icon icon-fb" href="#"><i class="mdi mdi-facebook-box"></i></a>
+
+                            <a class="nav-link nav-icon icon-twitter" href="#"><i class="mdi mdi-twitter-box"></i></a>
+    
+                            <a class="nav-link nav-icon icon-insta" href="#"><i class="mdi mdi-instagram"></i></a>
+                            
+                        </li>
+            
+                        <form action="#" class="form-inline ml-lg-3">
+                            <button class="btn btn-danger">Download Now</button>
+                        </form> -->
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div
+            class="miri-ui-kit-header-content text-center text-white d-flex flex-column justify-content-center align-items-center">
+            <!-- <h1 class="display-3 text-white">Landing Page</h1>
+            <p class="h3 font-weight-light text-white">A beautiful premium bootstrap 4 developed by bootstrapdash.</p>
+            <p class="mt-4"><button class="btn btn-danger btn-pill mr-2">Watch
+                    Video</button> <button class="btn btn-success btn-pill">Purchase Now</button></p> -->
+        </div>
+    </header>
